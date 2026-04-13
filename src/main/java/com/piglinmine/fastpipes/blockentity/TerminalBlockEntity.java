8f13241ebf0c6c61cf81ab5b05783875b5b50476
Big_Energy <andre90094@gmail.com>
2026-04-13 11:49:41 +0300

    Working recipes/loottables

--- src/main/java/com/piglinmine/fastpipes/blockentity/TerminalBlockEntity.java
+++ src/main/java/com/piglinmine/fastpipes/blockentity/TerminalBlockEntity.java
@@ -7,14 +7,37 @@ import net.minecraft.nbt.ListTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 
+import net.minecraft.core.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+import javax.annotation.Nullable;
 import java.util.Arrays;
 
 public class TerminalBlockEntity extends BaseBlockEntity {
     private final ItemStack[] craftGrid = new ItemStack[9];
+    private final LazyOptional<IItemHandler> itemHandler = LazyOptional.of(() -> new ItemStackHandler(0));
 
     public TerminalBlockEntity(BlockPos pos, BlockState state) {
         super(FPipesBlockEntities.TERMINAL.get(), pos, state);
         Arrays.fill(craftGrid, ItemStack.EMPTY);
+    }
+
+    @Override
+    public <T> LazyOptional<T> getCapability(Capability<T> cap, @Nullable Direction side) {
+        if (cap == ForgeCapabilities.ITEM_HANDLER) {
+            return itemHandler.cast();
+        }
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        itemHandler.invalidate();
     }
 
     @Override
