7f2682c302e7529e2f033e6369673785b6998cf6
Big_Energy <andre90094@gmail.com>
2026-06-20 09:17:04 +0300

    wip 1.21.11 fix blockitem prefix

--- src/main/java/com/piglinmine/fastpipes/item/BaseBlockItem.java
+++ src/main/java/com/piglinmine/fastpipes/item/BaseBlockItem.java
@@ -5,8 +5,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class BaseBlockItem extends BlockItem {
-    // 1.21.11: id is injected into Properties by DeferredRegister.Items.registerItem(...)
     public BaseBlockItem(Block block, Item.Properties properties) {
-        super(block, properties);
+        super(block, properties.useBlockDescriptionPrefix());
     }
 } 
